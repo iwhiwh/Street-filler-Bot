1e376622352384ba77803475560d50feb93930d9
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-09-12 11:28:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,1 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/iwhiwh/Street-filler-Bot /Street-filler-Bot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Street-filler-Bot
-fi
-cd /Auto-search-tamil-bot
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+gunicorn app:app & python3 bot.py
